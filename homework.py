be81ee37912e616a307a63b5358968a2bf032c58
Захаров Павел <pzakharov83@gmail.com>
2021-06-24 17:29:51 +0300

    Some adjustments

--- homework.py
+++ homework.py
@@ -14,6 +14,9 @@ TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 SERVER_ROUND_TRIP_TIMEOUT = 60 * 10
 SERVER_ERROR_REPEAT_TIMEOUT = SERVER_ROUND_TRIP_TIMEOUT
+API_URL_PRAKTIKUM = (
+    'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+)
 
 bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
@@ -28,27 +31,29 @@ def parse_homework_status(homework):
         'approved': 'Ревьюеру всё понравилось, работа зачтена!',
         'rejected': 'К сожалению, в работе нашлись ошибки.'
     }
-    if 'homework_name' in homework:
-        homework_name = homework['homework_name']
-        if 'status' in homework:
-            status = homework['status']
-            if status in statuses.keys():
-                return ('У вас проверили работу '
-                        f'"{homework_name}"!\n\n{statuses[status]}')
-            elif status == 'reviewing':
-                return f'Работа "{homework_name}" ушла на проверку'
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+
+    if homework is None and status is None:
+        return 'Неверный ответ сервера.'
+    else:
+        if status in statuses.keys():
+            return ('У вас проверили работу '
+                    f'"{homework_name}"!\n\n{statuses[status]}')
+        elif status == 'reviewing':
+            return f'Работа "{homework_name}" ушла на проверку'
     return 'Неверный ответ сервера.'
 
 
 def get_homeworks(current_timestamp=None):
     homework_statuses = []
-    headers = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
     if current_timestamp is None:
         current_timestamp = int(time.time())
     params = {'from_date': current_timestamp}
     try:
         homework_statuses = requests.get(
-            'https://praktikum.yandex.ru/api/user_api/homework_statuses/',
+            API_URL_PRAKTIKUM,
             headers=headers, params=params
         )
     except Exception as e:
@@ -75,6 +80,7 @@ def main():
                 if current_homework is not None:
                     message = parse_homework_status(current_homework)
                     logging.debug(message)
+                    current_timestamp = current_homework.get('current_date')
                     if status != message:
                         send_message(message)
                         status = message
